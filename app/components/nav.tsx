aa8601440f5f6b770c80f264de5ca5508fb16a32
santabes93 <santabes93@gmail.com>
2025-12-22 00:00:58 +0000

    add Profile component

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -40,6 +40,12 @@ export const Navigation: React.FC = () => {
 						>
 							Contact
 						</Link>
+						<Link
+							href="/profile"
+							className="duration-200 text-zinc-400 hover:text-zinc-100"
+						>
+							Profile
+						</Link>
 					</div>
 
 					<Link
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,7 +4,6 @@ import {allProjects} from "contentlayer/generated";
 import {Navigation} from "../components/nav";
 import {Card} from "../components/card";
 import {Article} from "./article";
-import {Eye} from "lucide-react";
 
 export const revalidate = 60;
 export default async function ProjectsPage() {
