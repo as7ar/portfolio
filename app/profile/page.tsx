6266b405e8afa08f034a91709cf363d0c4bc32fd
santabes93 <santabes93@gmail.com>
2025-12-22 04:46:32 +0000

    build structure of profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,26 @@
 import { Navigation } from "../components/nav";
+import "./profile.css";
 
 export default function Profile() {
-    return (
-        <>
-            <div>
-                <Navigation/>
-            </div>
-        </>
-    )
+  return (
+    <div className="profile-box">
+      <Navigation />
+
+      <div className="name-card">
+        <div className="card-bg" />
+
+        <div className="avatar" />
+        <div className="name" />
+
+        <div className="title" />
+        <div className="description" />
+
+        <div className="label" />
+        <div className="line line-1" />
+        <div className="line line-2" />
+        <div className="line line-3" />
+        <div className="line line-4" />
+      </div>
+    </div>
+  );
 }
